6ab92a9c7fa2b573cb7a8dae08d22fb4ce29e1d2
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2020-02-19 18:25:56 -0800

    More Array Stuffs, cool.

--- src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
+++ src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
@@ -4,20 +4,39 @@ import java.util.ArrayList;
 
 public class _01_IntroToArrayLists {
 	public static void main(String[] args) {
-		//1. Create an array list of Strings
-		//   Don't forget to import the ArrayList class
+		// 1. Create an array list of Strings
+		// Don't forget to import the ArrayList class
+		ArrayList<String> strings = new ArrayList<String>();
 
-		//2. Add five Strings to your list
-		
-		//3. Print all the Strings using a standard for-loop
-		
-		
-		//4. Print all the Strings using a for-each loop
-		
-		//5. Print only the even numbered elements in the list.
-		
-		//6. Print all the Strings in reverse order.
-		
-		//7. Print only the Strings that have the letter 'e' in them.
+		// 2. Add five Strings to your list
+		for (int i = 0; i < 5; i++) {
+			strings.add("hello " + i);
+			System.out.println(strings.get(i));
+		}
+
+		// 3. Print all the Strings using a standard for-loop
+
+		// 4. Print all the Strings using a for-each loop
+
+		for (String string : strings) {
+			System.out.println(string);
+		}
+
+		// 5. Print only the even numbered elements in the list.
+		for (int i = 0; i < strings.size(); i += 2) {
+			System.out.println(strings.get(i));
+		}
+		// 6. Print all the Strings in reverse order.
+		for (int i = strings.size() - 1; i > 0; i--) {
+			System.out.println(strings.get(i));
+		}
+		// 7. Print only the Strings that have the letter 'e' in them.
+		strings.add("nam");
+
+		for (int i = 0; i < strings.size(); i++) {
+			if (strings.get(i).contains("e")) {
+				System.out.println(strings.get(i));
+			}
+		}
 	}
 }
--- src/_01_IntroToArrayLists/_02_GuestBook.java
+++ src/_01_IntroToArrayLists/_02_GuestBook.java
@@ -1,15 +1,62 @@
 package _01_IntroToArrayLists;
 
-public class _02_GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
-	// all the names added to the list. Format the list as follows:
-	// Guest #1: Bob Banders
-	// Guest #2: Sandy Summers
-	// Guest #3: Greg Ganders
-	// Guest #4: Donny Doners
-	
-	
-	
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_GuestBook implements ActionListener {
+	// Create a GUI with two buttons. One button reads "Add Name" and the other
+	// button reads "View Names"
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button1 = new JButton("Add name");
+	JButton button2 = new JButton("View names");
+
+	ArrayList<String> names = new ArrayList<String>();
+
+	void createGUI() {
+		frame.setVisible(true);
+		frame.add(panel);
+		panel.add(button1);
+		button1.addActionListener(this);
+		panel.add(button2);
+		button2.addActionListener(this);
+		frame.pack();
+	}
+
+	public static void main(String[] args) {
+		_02_GuestBook main = new _02_GuestBook();
+		main.createGUI();
+
+		// When the add name button is clicked, display an input dialog that asks the
+		// user to enter a name. Add
+		// that name to an ArrayList. When the "View Names" button is clicked, display a
+		// message dialog that displays
+		// all the names added to the list. Format the list as follows:
+		// Guest #1: Bob Banders
+		// Guest #2: Sandy Summers
+		// Guest #3: Greg Ganders
+		// Guest #4: Donny Doners
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == button1) {
+			String nameInput = JOptionPane.showInputDialog("Enter a name.");
+			names.add(nameInput);
+		} else if(e.getSource() == button2) {
+			String list = "";
+			for (int i = 0; i < names.size(); i++) {
+				list = list + "Guest #1: " + names.get(i) + "\n";
+			}
+			JOptionPane.showMessageDialog(null, list);
+		}
+	}
+
 }
